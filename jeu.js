6aa0cf51cf996b50b8d48955b3b39976a4e0dfb5
Walter <zanderlavastory@gmail.com>
2024-09-12 13:56:36 -0400

    Added mouse gestures for player 1

--- jeu.js
+++ jeu.js
@@ -1,3 +1,18 @@
+// ====================================== Classes Definition ======================================
+class LightCycle {
+    x = NUM_CELLS_HORIZONTAL / 2;
+    y = 0;
+    vx = 0; // positive for right
+    vy = 0; // positive for down
+    alive = true;
+}
+
+class Point2D {
+    x = 0;
+    y = 0;
+}
+
+// ====================================== Canvas Setup ======================================
 // Creates a 2D array filled with zeros
 var create2DArray = function (numColumns, numRows) {
     var array = [];
@@ -8,9 +23,7 @@ var create2DArray = function (numColumns, numRows) {
         }
     }
     return array;
-}
-
-
+};
 
 var canvas = document.getElementById("myCanvas");
 var C = canvas.getContext("2d");
@@ -27,62 +40,94 @@ var grid = create2DArray(NUM_CELLS_HORIZONTAL, NUM_CELLS_VERTICAL);
 var CELL_EMPTY = 0;
 var CELL_OCCUPIED = 1;
 
-class LightCycle {
-    x = NUM_CELLS_HORIZONTAL / 2
-    y = 0
-    vx = 0 // positive for right
-    vy = 0 // positive for down
-    alive = true
-}
+// ====================================== Game Setup ======================================
+// Mouse Position
+const mouseDownPos = new Point2D();
+const mouseUpPos = new Point2D();
+var mouseDownInCanvas = false; // Indicates that the mouse down happened inside the canvas
 
 // Current position and direction of light cycle 1
-const lightCycle1 = new LightCycle()
-lightCycle1.y = NUM_CELLS_VERTICAL - 2
-lightCycle1.vy = -1
+const lightCycle1 = new LightCycle();
+lightCycle1.y = NUM_CELLS_VERTICAL - 2;
+lightCycle1.vy = -1;
 
 // Current y-position and y-direction of light cycle 2
-const lightCycle2 = new LightCycle()
-lightCycle2.y = 2
-lightCycle2.vy = 1
+const lightCycle2 = new LightCycle();
+lightCycle2.y = 1;
+lightCycle2.vy = 1;
 
 grid[lightCycle1.x][lightCycle1.y] = CELL_OCCUPIED; // to mark the initial grid cell as occupied
 grid[lightCycle2.x][lightCycle2.y] = CELL_OCCUPIED; // to mark the initial grid cell as occupied
 
+// ====================================== Controls ======================================
 function keyDownHandler(e) {
     // console.log("keyCode: " + e.keyCode );
     // e = e || window.event;
-    const arrowKeys = [38, 40, 37, 39]
-    const wasdKeys = [87, 83, 65, 68]
-    const keyCode = e.keyCode
 
-    let vx = 0
-    let vy = 0
+    const arrowKeys = [38, 40, 37, 39];
+    const wasdKeys = [87, 83, 65, 68];
+    const keyCode = e.keyCode;
 
-    if (keyCode === arrowKeys[0] || keyCode === wasdKeys[0]) { // up
+    let vx = 0;
+    let vy = 0;
+
+    if (keyCode === arrowKeys[0] || keyCode === wasdKeys[0]) {
+        // up
         vy = -1;
-    }
-    else if (keyCode === arrowKeys[1] || keyCode === wasdKeys[1]) { // down
+    } else if (keyCode === arrowKeys[1] || keyCode === wasdKeys[1]) {
+        // down
         vy = 1;
-    }
-    else if (keyCode === arrowKeys[2] || keyCode === wasdKeys[2]) { // left
+    } else if (keyCode === arrowKeys[2] || keyCode === wasdKeys[2]) {
+        // left
         vx = -1;
-    }
-    else if (keyCode === arrowKeys[3] || keyCode === wasdKeys[3]) { // right
+    } else if (keyCode === arrowKeys[3] || keyCode === wasdKeys[3]) {
+        // right
         vx = 1;
     }
 
-    if (arrowKeys.includes(keyCode)) { // Player 1
+    if (arrowKeys.includes(keyCode)) {
+        // Player 1
         lightCycle1.vx = vx;
         lightCycle1.vy = vy;
-    } else if (wasdKeys.includes(keyCode)) { // Player 2
+    } else if (wasdKeys.includes(keyCode)) {
+        // Player 2
         lightCycle2.vx = vx;
         lightCycle2.vy = vy;
     }
 }
 
+function mouseDownHandler(e) {
+    mouseDownPos.x = e.screenX;
+    mouseDownPos.y = e.screenY;
+    mouseDownInCanvas = true;
+}
+
+function mouseUpHandler(e) {
+    if (mouseDownInCanvas) {
+        mouseUpPos.x = e.screenX;
+        mouseUpPos.y = e.screenY;
+
+        const delta_x = mouseUpPos.x - mouseDownPos.x;
+        const delta_y = mouseUpPos.y - mouseDownPos.y;
+
+        lightCycle1.vx = 0;
+        lightCycle1.vy = 0;
+
+        if (Math.abs(delta_x) > Math.abs(delta_y)) {
+            lightCycle1.vx = delta_x > 0 ? 1 : -1; // if true, vers la droite, else vers la gauche
+        } else {
+            lightCycle1.vy = delta_y > 0 ? 1 : -1; // if true, vers le bas, else vers le haut
+        }
+
+        mouseDownInCanvas = false; // resets the variable
+    }
+}
+
+myCanvas.onmousedown = mouseDownHandler;
 document.onkeydown = keyDownHandler;
+document.onmouseup = mouseUpHandler;
 
-
+// ====================================== LightCycle Updates ======================================
 var redraw = function () {
     C.fillStyle = "#000000";
     // C.clearRect(0, 0, canvas.width, canvas.height);
@@ -92,24 +137,38 @@ var redraw = function () {
     for (var i = 0; i < NUM_CELLS_HORIZONTAL; ++i) {
         for (var j = 0; j < NUM_CELLS_VERTICAL; ++j) {
             if (grid[i][j] === CELL_OCCUPIED)
-                C.fillRect(x0 + i * cellSize + 1, y0 + j * cellSize + 1, cellSize - 2, cellSize - 2);
+                C.fillRect(
+                    x0 + i * cellSize + 1,
+                    y0 + j * cellSize + 1,
+                    cellSize - 2,
+                    cellSize - 2
+                );
         }
     }
 
     const getHeadColor = (lightCycle) => {
         C.fillStyle = lightCycle.alive ? "#ff0000" : "#ffffff";
-        C.fillRect(x0 + lightCycle.x * cellSize, y0 + lightCycle.y * cellSize, cellSize, cellSize);
-    }
+        C.fillRect(
+            x0 + lightCycle.x * cellSize,
+            y0 + lightCycle.y * cellSize,
+            cellSize,
+            cellSize
+        );
+    };
 
-    getHeadColor(lightCycle1) // Player 1
-    getHeadColor(lightCycle2) // Player 2
-}
+    getHeadColor(lightCycle1); // Player 1
+    getHeadColor(lightCycle2); // Player 2
+};
 
 var hasCollided = function (new_x, new_y) {
-    return new_x < 0 || new_x >= NUM_CELLS_HORIZONTAL
-        || new_y < 0 || new_y >= NUM_CELLS_VERTICAL
-        || grid[new_x][new_y] === CELL_OCCUPIED
-}
+    return (
+        new_x < 0 ||
+        new_x >= NUM_CELLS_HORIZONTAL ||
+        new_y < 0 ||
+        new_y >= NUM_CELLS_VERTICAL ||
+        grid[new_x][new_y] === CELL_OCCUPIED
+    );
+};
 
 var updateLightCycle = function (lightCycle) {
     var new_x = lightCycle.x + lightCycle.vx;
@@ -118,20 +177,21 @@ var updateLightCycle = function (lightCycle) {
     // Check for collision with grid boundaries and with trail
     if (hasCollided(new_x, new_y)) {
         lightCycle.alive = false;
-    }
-    else {
+    } else {
         grid[new_x][new_y] = CELL_OCCUPIED;
         lightCycle.x = new_x;
         lightCycle.y = new_y;
     }
-}
+};
 
 var advance = function () {
     if (lightCycle1.alive && lightCycle2.alive) {
-        updateLightCycle(lightCycle1)
-        updateLightCycle(lightCycle2)
+        updateLightCycle(lightCycle1);
+        updateLightCycle(lightCycle2);
         redraw();
     }
-}
+};
 
-setInterval(function () { advance(); }, 100 /*milliseconds*/);
+setInterval(function () {
+    advance();
+}, 100 /*milliseconds*/);
